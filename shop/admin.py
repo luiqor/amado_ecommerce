b930eeeae3d5f5d4659e5de6e50c0f5325e738bf
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-01 21:59:46 +0300

    added admin registrations for brand, item, category models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Brand, Item
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_field = {'slug':('name',)}
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'photo', 'description', 
+                    'quantity', 'stars', 'category', 'brand']
+    list_filter = ['category', 'brand']
+    ordering = ['price','-price', 'created', '-created', 'stars', '-stars']
+    list_editable = ['name','price', 'photo', 'description', 
+                     'quantity', 'stars']
+    prepopulated_fields = {'slug': ('name',)}

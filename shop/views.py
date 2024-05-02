f878deef8407d357e9d981e04da8c67dd87655b5
Diana Vashchilina <vohnenna@gmail.com>
2024-05-02 15:39:44 +0300

    implement items display on shop page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Category, Brand
+from .models import Category, Brand, Item
 
 
 def home(request):
@@ -7,10 +7,16 @@ def home(request):
 
 
 def shop(request):
+    items = Item.objects.all()
     categories = Category.objects.all()
     brands = Brand.objects.all()
+
+    for item in items:
+        item.stars_range = range(item.stars)
+        item.empty_stars_range = range(5 - item.stars)
+
     return render(
         request,
         "shop.html",
-        context={"categories": categories, "brands": brands},
+        context={"categories": categories, "brands": brands, "items": items},
     )

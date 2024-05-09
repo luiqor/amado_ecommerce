57fccb33ee1409e78054d066bece3c597f47e834
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-09 14:40:39 +0300

    fix item page gty

--- static/script.js
+++ static/script.js
@@ -1,3 +1,7 @@
+function updatePage() {
+    location.reload(); 
+}
+
 
 function increaseQuantity() {
     var effect = document.getElementById('qty');
@@ -8,6 +12,7 @@ function increaseQuantity() {
     }
 }
 
+
 function decreaseQuantity() {
     var effect = document.getElementById('qty');
     var qty = parseInt(effect.value);
@@ -16,6 +21,7 @@ function decreaseQuantity() {
         updateQuantity();
     }
 }
+
 
 function updateQuantity() {
     var effect = document.getElementById('qty');
@@ -34,13 +40,16 @@ function adjustQuantity(itemId, change) {
     }
 }
 
+
 function increaseQuantityConcret(itemId) {
     adjustQuantity(itemId, 1);
 }
 
+
 function decreaseQuantityConcret(itemId) {
     adjustQuantity(itemId, -1);
 }
+
 
 function updateQuantityConcret(itemId) {
     var effect = document.getElementById('qty-' + itemId);
@@ -50,86 +59,3 @@ function updateQuantityConcret(itemId) {
     }
 }
 
-
-// Add Item
-  $(document).on("click", "#add-button", function (e) {
-    e.preventDefault();
-    var addToCartUrl = '{% url "cart:cart_add" %}';
-    console.log(addToCartUrl)
-    var itemid = $(this).val();
-    var itemqty = $("#qty").val();
-    console.log(itemid, itemqty)
-    $.ajax({
-      type: "POST",
-      url: addToCartUrl,
-      data: {
-        itemid: itemid,
-        itemqty: itemqty,  
-        csrfmiddlewaretoken: "{{ csrf_token }}",
-        action: "post",
-      },
-      success: function (json) {
-        if (json) { 
-          document.getElementById("cart-qty").innerHTML = json.qty;
-          console.log("Товар успішно додано у кошик");
-        } else {
-          console.log("Помилка: товар не було додано у кошик");
-        }
-      },
-      error: function (xhr, errmsg, err) {
-        console.log("Помилка: не вдалося відправити запит на сервер");
-      },
-    });
-  });
-
-
-  // Update Item
-  $(document).on("click", ".update-button", function (e) {
-    e.preventDefault();
-    var prodid = $(this).data("index");
-    console.log(prodid);
-    var itemQty = $("#qty-" + prodid).val();
-    console.log("Item Quantity:", itemQty);
-    $.ajax({
-      type: "POST",
-      url: '{% url "cart:cart_update" %}',
-      data: {
-        itemid: prodid,
-        itemqty: itemQty,
-        csrfmiddlewaretoken: "{{csrf_token}}",
-        action: "post",
-      },
-      success: function (json) {
-        console.log("Success:", json);
-        document.getElementById("cart-qty").innerHTML = json.qty;
-        document.getElementById("subtotal").innerHTML = json.subtotal;
-      },
-      error: function (xhr, errmsg, err) {
-        console.log("Error:", errmsg);
-      },
-    });
-  });
-
-
-   // Delete Item
-  $(document).on('click', '.delete-button', function (e) {
-    e.preventDefault();
-    var prodid = $(this).data("index");
-    console.log("id", prodid)
-    $.ajax({
-      type: 'POST',
-      url: '{% url "cart:cart_delete" %}',
-      data: {
-       itemid: $(this).data('index'),
-        csrfmiddlewaretoken: "{{csrf_token}}",
-        action: 'post'
-      },
-      success: function (json) {
-        $('.this-item[data-index="' + prodid + '"]').remove();
-        document.getElementById("cart-qty").innerHTML = json.qty
-        document.getElementById("subtotal").innerHTML = json.subtotal;
-        
-      },
-      error: function (xhr, errmsg, err) {}
-    });
-  })

30b605ace4f613a490e5089f4e8a6c5f5e80a1b5
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-15 11:16:36 +0300

    added python-html and flake8-html. fixed flake8

--- shop/admin.py
+++ shop/admin.py
@@ -12,17 +12,17 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ['id','name', 'slug']
+    list_display = ['id', 'name', 'slug']
     list_editable = ['name']
     search_fields = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['id','name', 'slug', 'price', 'photo', 'description', 
+    list_display = ['id', 'name', 'slug', 'price', 'photo', 'description',
                     'quantity', 'in_stock', 'stars', 'category', 'brand']
-    list_filter = ['name','category', 'brand', 'in_stock',]
-    list_editable = ['price', 'in_stock', 'photo', 'description', 
+    list_filter = ['name', 'category', 'brand', 'in_stock',]
+    list_editable = ['price', 'in_stock', 'photo', 'description',
                      'quantity', 'stars']
     prepopulated_fields = {'slug': ('name',)}

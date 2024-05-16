9c19ca5eb5f7c246014b34f2250a1cec9b2bcf61
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-16 17:46:49 +0300

    added script to disable button on item page when there are not enough items in db

--- static/script.js
+++ static/script.js
@@ -1,60 +1,34 @@
 function updatePage() {
-    location.reload(); 
-}
-
-
-function increaseQuantity() {
-    var effect = document.getElementById('qty');
-    var qty = parseInt(effect.value);
-    if (!isNaN(qty)) {
-        effect.value = qty + 1;
-        updateQuantity();
-    }
-}
-
-
-function decreaseQuantity() {
-    var effect = document.getElementById('qty');
-    var qty = parseInt(effect.value);
-    if (!isNaN(qty) && qty > 1) {
-        effect.value = qty - 1;
-        updateQuantity();
-    }
-}
-
-
-function updateQuantity() {
-    var effect = document.getElementById('qty');
-    var qty = parseInt(effect.value);
-}
+    location.reload();
+};
 
 
 function adjustQuantity(itemId, change) {
-    var effect = document.getElementById('qty-' + itemId);
-    var qty = parseInt(effect.value);
+    let effect = document.getElementById('qty-' + itemId);
+    let qty = parseInt(effect.value);
     if (!isNaN(qty)) {
-        var newQty = qty + change;
+        let newQty = qty + change;
         if (newQty >= 1) {
             effect.value = newQty;
-        }
-    }
-}
+        };
+    };
+};
 
 
 function increaseQuantityConcret(itemId) {
     adjustQuantity(itemId, 1);
-}
+};
 
 
 function decreaseQuantityConcret(itemId) {
     adjustQuantity(itemId, -1);
-}
+};
 
 
 function updateQuantityConcret(itemId) {
-    var effect = document.getElementById('qty-' + itemId);
-    var qty = parseInt(effect.value);
+    let effect = document.getElementById('qty-' + itemId);
+    let qty = parseInt(effect.value);
     if (!isNaN(qty)) {
         console.log("Нове значення для елемента з id", itemId + ":", qty);
-    }
-}
+    };
+};
